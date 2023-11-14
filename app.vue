ae6c908a9e5c57a04d830cf2bb56c5c87d4f32b3
RYU0706 <146798739+RYU0706@users.noreply.github.com>
2023-11-14 16:47:39 +0900

    up

--- app.vue
+++ app.vue
@@ -1,27 +1,5 @@
 <template>
-  <div id="app">
-    <OthelloBoard />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
-
-<script>
-import OthelloBoard from './components/OthelloBoard.vue'
-
-export default {
-  name: 'App',
-  components: {
-    OthelloBoard
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
